3549b0e710e864c045c2d325419e89be734d0947
Christian Varisco <vari.chri1993@gmail.com>
2017-12-14 00:47:28 +0100

    Add first test of api

--- src/coinbaseApi.js
+++ src/coinbaseApi.js
@@ -1,19 +1,49 @@
 import { Client } from 'coinbase';
 
 const coinbaseClient = (() => {
-  const client = new Client();
+  const client = new Client({
+    apiKey: 'API KEY',
+    apiSecret: 'API SECRET',
+    strictSSL: false,
+  });
 
+  /**
+   * 
+   * @param {Object} response from the API
+   */
+  const cleanResponse = (response) => {
+    if (response.data) {
+      return response.data;
+    }
+    return response;
+  }
+
+  /**
+   * Functions that handle all the API from the coinbase lib 
+   * @param {Func} resolve 
+   * @param {Func} reject 
+   */
+  const responseHandler = (resolve, reject) => (err, res) => {
+    if (err) {
+      return reject(err)
+    }
+    return resolve(cleanResponse(res))
+  }
+
+  /**
+   * Get the current market price for bitcoin. This is usually somewhere in between the buy and sell price.
+   * @param {String} currencyPair pair of currencies, example BTC-EUR
+   */
   const getSpotPrice = (currencyPair) => {
     return new Promise((resolve, reject) => {
-      client.getSpotPrice({ currencyPair }, (err, price) => {
-        if (err) {
-          return reject(err)
-        }
-        return resolve(price)
-      });
+      client.getSpotPrice({ currencyPair }, responseHandler(resolve, reject));
     });
   }
-})
+
+  return {
+    getSpotPrice
+  }
+})()
 
 
 export default coinbaseClient
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './popup.css';
 import coinbaseApi from './coinbaseApi';
 
-coinbaseApi.getSpotPrice('BTC-EUR').then((price) => {
-  console.log(price)
-})
+const one = coinbaseApi.getSpotPrice('LTC-EUR');
+const two = coinbaseApi.getSpotPrice('BTC-EUR');
+
+
+Promise.all([one,
+  two]).then((data) => { console.log(data) })
