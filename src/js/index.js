0cc2ffec8c69182fba7558b4dbdb0e010455a55f
Christian Varisco <vari.chri1993@gmail.com>
2017-12-27 09:28:36 +0100

    Add comments

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,41 @@
+/**
+ * Should be refactored ASAP
+ */
+
 import coinbaseApi from './services/coinbaseApi'
 import generateProduct from './templates/products'
 import PRODUCTS from './constants/products'
 
-function addProduct(product, res) {
+const addProductToHTML = product => (res) => {
   const productTemplate = generateProduct(product, res)
   document.getElementById('products').insertAdjacentHTML('beforeend', productTemplate)
 }
 
-function main() {
-  const products = Object.keys(PRODUCTS).map(productName => ({
+function addProduct(products) {
+  products.forEach((product) => {
+    Promise.all(product.apiCalls).then(addProductToHTML(product))
+  })
+}
+
+function generateProductStructure() {
+  return Object.keys(PRODUCTS).map(productName => ({
     ...PRODUCTS[productName],
     apiCalls: PRODUCTS[productName].currencyPairs.map(currencyPair =>
       coinbaseApi.getSpotPrice(currencyPair)
     ),
   }))
-
-  products.forEach((product) => {
-    Promise.all(product.apiCalls).then((res) => {
-      addProduct(product, res)
-    })
-  })
 }
 
-window.onload = main
+function main() {
+  const products = generateProductStructure()
+
+  addProduct(products)
+}
+
+// TODO: Refactor the logic of FETCH and create products
+// I put this bad setTimeout because of this: https://github.com/CVarisco/crypto-chrome-ext/issues/1
+window.onload = () => {
+  setTimeout(() => {
+    main()
+  }, 500)
+}
--- src/js/templates/products.js
+++ src/js/templates/products.js
@@ -1,5 +1,10 @@
 import getCurrencySymbol from '../utils'
 
+/**
+ * Generate the product item
+ * @param {Object} data contains information about the product
+ * @return {String} li html element that contains the product info
+ */
 const generateProductListItem = ({ base, currency, amount }) => `
 <li class="product-list-item">
   <span class="item-title">${base}/${currency}</span>
@@ -9,12 +14,21 @@ const generateProductListItem = ({ base, currency, amount }) => `
 </li>
 `
 
+/**
+ * Generate the product item error
+ * @return {String} li html element that contains the item error
+ */
 const generateProductListItemError = () => `
 <li class="product-list-item">
  <span class="item-title">Error fetching result</span>
 </li>
 `
 
+/**
+ * Generate the list of product items
+ * @param {Array} res contains all the response from the API
+ * @return {String} All the products items generated
+ */
 const generateProductListItems = (res) => {
   const templates = res.map(({ data }) => {
     if (res.errors) {
@@ -26,6 +40,12 @@ const generateProductListItems = (res) => {
   return templates.join('')
 }
 
+/**
+ * Generate product item (crypto currency) that contains all the prices
+ * @param {Object} product all the informations about the product (crypto currency)
+ * @param {Array} res contains all the response from the API
+ * @return {String} entiry product template
+ */
 const generateProduct = (product, res) => `
 <li class="product ${product.id}">
   <h2 class="product-title"><img class="logo" src="./assets/images/${product.id}.svg" />
